2d198e1550229a1e02253954e03dbe91fc66cb54
Mark Lilback <mark@lilback.com>
2019-12-13 11:31:01 -0500

    made test code more complex

--- Tests/Rc2ParserTests/RParserTests.swift
+++ Tests/Rc2ParserTests/RParserTests.swift
@@ -12,7 +12,7 @@ final class RParserTests: XCTestCase {
 	let basicText = #"""
 	# some code
 	z <- 2
-	rnorm(x,y)
+	rnorm(x,plot(1,"dude"))
 	"""#
 		
 	func testBasicHighlight() throws {
@@ -20,10 +20,18 @@ final class RParserTests: XCTestCase {
 		let parser = RmdParser();
 		let str = NSMutableAttributedString(string: basicText)
 		try parser.highlight(content: str)
-		str.enumerateAttribute(RmdParser.SyntaxKey, in: NSRange(location: 0, length: basicText.count), options: []) { (keyValue, attrRange, _) in
-			guard let element = keyValue as? RmdParser.SyntaxElement else { return }
-			print("got \(element.rawValue) for \(str.attributedSubstring(from: attrRange).string)")
+		var keys = [SyntaxElement]()
+		str.enumerateAttribute(SyntaxKey, in: NSRange(location: 0, length: basicText.count), options: []) { (keyValue, attrRange, _) in
+			guard let element = keyValue as? SyntaxElement else { return }
+			keys.append(element)
+			// print("got \(element.rawValue) for \(str.attributedSubstring(from: attrRange).string)")
 		}
+		XCTAssertEqual(keys[0], SyntaxElement.comment)
+		XCTAssertEqual(keys[1], SyntaxElement.number)
+		XCTAssertEqual(keys[2], SyntaxElement.keyword)
+		XCTAssertEqual(keys[3], SyntaxElement.keyword)
+		XCTAssertEqual(keys[4], SyntaxElement.number)
+		XCTAssertEqual(keys[5], SyntaxElement.quote)
 	}
 	
 	static var allTests = [
